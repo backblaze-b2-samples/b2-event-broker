a292e73c9cca9a25a7acbf28fb6395acd6851dde
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2026-08-17 23:32:47 -0500

    fix: normalize subscription maps

--- src/index.js
+++ src/index.js
@@ -74,6 +74,14 @@ function checkProperty(obj, objType, property, id) {
 	}
 }
 
+function toPrototypeLessObject(value) {
+	const object = Object.create(null);
+	if (value && typeof value === 'object' && !Array.isArray(value)) {
+		Object.assign(object, value);
+	}
+	return object;
+}
+
 /** A Durable Object's behavior is defined in an exported Javascript class */
 export class EventSubscriptions extends DurableObject {
 	constructor(ctx, env) {
@@ -87,10 +95,8 @@ export class EventSubscriptions extends DurableObject {
 		if (!URL.canParse(subscription.url)) {
 			throw new Error('url in payload is not valid.');
 		}
-		const rules = (await this.ctx.storage.get(bucketName)) || Object.create(null);
-		const subscriptions = Object.hasOwn(rules, ruleName)
-			? rules[ruleName]
-			: Object.create(null);
+		const rules = toPrototypeLessObject(await this.ctx.storage.get(bucketName));
+		const subscriptions = toPrototypeLessObject(rules[ruleName]);
 		const id = uuidv4();
 		subscriptions[id] = {
 			url: subscription.url
@@ -143,8 +149,8 @@ export class EventSubscriptions extends DurableObject {
 			checkUUID(id);
 			checkProperty(subscription, 'subscription', 'url', id);
 		}
-		const rules = await this.ctx.storage.get(bucketName);
-		rules[ruleName] = subscriptions;
+		const rules = toPrototypeLessObject(await this.ctx.storage.get(bucketName));
+		rules[ruleName] = toPrototypeLessObject(subscriptions);
 		await this.ctx.storage.put(bucketName, rules);
 		console.log(`Updated subscriptions for ${bucketName}/${ruleName}`)
 	}
